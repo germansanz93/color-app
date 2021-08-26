ea25c16b5fec75b4fc689ee41dcdddf97ad77792
germansanz93 <germansanz93@gmail.com>
2021-08-26 15:53:05 -0300

    paletteLists and links, withstyles installed, start styling minipalette

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { IconButton, Select, MenuItem, Snackbar } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
 import Slider from 'rc-slider';
@@ -25,7 +26,7 @@ class NavBar extends Component {
     return (
       <header className='NavBar'>
         <div className='logo'>
-          <a href='#'>reactcolorpicker</a>
+          <Link to={'/'}>reactcolorpicker</Link>
         </div>
         <div className='slider-container'>
           <span>Level: {level}</span>
