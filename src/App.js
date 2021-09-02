9d4cd704ff872c85d2a938480f559a946bda3316
germansanz93 <germansanz93@gmail.com>
2021-09-02 20:58:28 -0300

    Add NewPaletteForm and Link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import PaletteList from './PaletteList';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 import SingleColorPalette from './SingleColorPalette';
+import NewPaletteForm from './NewPaletteForm';
 class App extends Component {
   findPalette(id) {
     return seedColors.find((palette) => palette.id === id);
@@ -12,6 +13,11 @@ class App extends Component {
   render() {
     return (
       <Switch>
+        <Route 
+          exact
+          path='/palette/new'
+          render={() => <NewPaletteForm/>}
+        />
         <Route
           exact
           path='/palette/:paletteId/:colorId'
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react'
 import MiniPalette from './MiniPalette';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles/PaletteListStyles';
+import { Link } from 'react-router-dom';
 class PaletteList extends Component {
-  goToPalette(id){
+  goToPalette(id) {
     this.props.history.push(`/palette/${id}`);
   }
   render() {
@@ -13,6 +14,7 @@ class PaletteList extends Component {
         <div className={classes.container}>
           <nav className={classes.nav}>
             <h1>React Colors</h1>
+            <Link to="/palette/new">Create Palette</Link>
           </nav>
           <div className={classes.palettes}>
             {palettes.map((palette) => (
